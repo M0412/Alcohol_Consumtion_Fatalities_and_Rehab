af8a136ce873b788b641d3ef9a3c22e3f4ca4580
Shipra Gupta <Shipra@Siddarthas-MBP.hsd1.ca.comcast.net>
2023-09-23 17:22:54 -0700

    merge

--- static/js/Rehab_logic.js
+++ static/js/Rehab_logic.js
@@ -14,9 +14,17 @@ const data = "../static/Data/JSON_Files/New_Rehab_Facilities_by_State.json";
 // Console log our data
 d3.json(data).then(function(response){
     // Create a new marker cluster group.
-    let markers = L.markerClusterGroup();
-    console.log(response)
+    let markers = L.markerClusterGroup({'animate': false});
+    //console.log(response)
+    console.log(response.length);
 
+    // specify popup options 
+    const customOptions = {
+        'maxWidth': '400',
+        'width': '200',
+        'className' : 'popupCustom'
+    };
+    var facility_count = 0;
     // Loop through the data.
     for (let i = 0; i < response.length; i++) {
 
@@ -25,36 +33,35 @@ d3.json(data).then(function(response){
 
         // loop through facility array 
         for (let j = 0; j < facilityList.length; j++){
-
+            facility_count++;
             // for each facility Set the data location property to a variable
-            let location1 = facilityList[j].Longitude;
-            let location2 = facilityList[j].Latitude;
-            console.log(location1,location2)
+            let location_long = facilityList[j].Longitude;
+            let location_lat = facilityList[j].Latitude;
+            //console.log(location1,location2)
 
             // Check for the location property.
-            if (location1 && location2) {
-                console.log("found marker")
+            if (location_long && location_lat) {
+                //console.log("found marker")
 
-                var popup = "<h4>" + facilityList[j].name1 + "</h4><hr><h5>" + facilityList[j].street1 + ",</h5><h5>" + facilityList[j].city+ ", " + response[i].state_name + ", " + facilityList[j].zip + "</h5>"
-
-                // specify popup options 
-                var customOptions =
-                {'maxWidth': '400',
-                'width': '200',
-                'className' : 'popupCustom'}
+                var popup = ("<h4>" + facilityList[j].name1 
+                            + "</h4><hr><h5>" + facilityList[j].street1 
+                            + ",</h5><h5>" + facilityList[j].city 
+                            + ", " + response[i].state_name 
+                            + ", " + facilityList[j].zip 
+                            + "</h5>");
 
                 // Add a new marker to the cluster group, and bind a popup.
-                markers.addLayer(L.marker([location2, location1])
+                markers.addLayer(L.marker([location_lat, location_long])
                 .bindPopup(popup,customOptions));
             };
         };
         
         
     };
-    
+    console.log(facility_count);
     // Add our marker cluster layer to the map.
     map.addLayer(markers);
-    
+    console.log("rehab line 58");
     // var evt = document.createEvent('UIEvents');
     // evt.initUIEvent('resize', true, false, window, 0);
     // window.dispatchEvent(evt);
